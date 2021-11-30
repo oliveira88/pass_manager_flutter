875ee10ec078a1dc3085f58696b7e58f582fbb69
Guilherme Oliveira <guilherme.oliveira@itixti.com.br>
2021-11-30 14:58:54 -0300

    Adição do acesso à lista de contas do store

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,38 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:pass_manager/model/account_list.dart';
 import 'package:pass_manager/screens/add_account.dart';
 import 'package:pass_manager/widgets/card_manager.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Gerenciador de Senhas'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ListView(
-          children: const [
-            CardManager(
-              title: 'Teste',
+    final accounts = Provider.of<AccountList>(context);
+    return Observer(
+      builder: (_) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('Gerenciador de Senhas'),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListView.builder(
+              itemCount: accounts.accounts.length,
+              itemBuilder: (_, index) {
+                final account = accounts.accounts[index];
+                return CardManager(title: account.name);
+              },
             ),
-            CardManager(
-              title: 'Facebook',
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => AddPassword()),
-          );
-        },
-      ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => AddPassword()),
+              );
+            },
+          ),
+        );
+      },
     );
   }
 }
