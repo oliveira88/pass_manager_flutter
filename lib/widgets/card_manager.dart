ee304eff508eaa449df0b310480d23507a61f8d6
Guilherme Oliveira <gui.r.oliveira.88@gmail.com>
2021-12-01 09:29:16 -0300

    Adição da funcionalidade de excluir conta

--- lib/widgets/card_manager.dart
+++ lib/widgets/card_manager.dart
@@ -1,14 +1,50 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:pass_manager/model/account.dart';
+import 'package:pass_manager/model/account_list.dart';
+import 'package:provider/provider.dart';
 
 class CardManager extends StatelessWidget {
-  const CardManager({Key? key, required this.title}) : super(key: key);
-  final String title;
+  const CardManager({Key? key, required this.account}) : super(key: key);
+  final Account account;
   @override
   Widget build(BuildContext context) {
+    final accounts = Provider.of<AccountList>(context);
     return Card(
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          print(account.password);
+        },
+        onLongPress: () {
+          showMenu(
+            context: context,
+            position: RelativeRect.fromLTRB(1, 165, 0, 0),
+            items: [
+              PopupMenuItem(
+                height: 40,
+                child: Text(
+                  'Editar',
+                  textAlign: TextAlign.center,
+                ),
+                onTap: () {
+                  print('EDITAR');
+                },
+              ),
+              PopupMenuItem(
+                height: 40,
+                child: Text(
+                  'Excluir',
+                  style: TextStyle(color: Colors.red),
+                  textAlign: TextAlign.center,
+                ),
+                onTap: () {
+                  accounts.removeAccount(account);
+                },
+              ),
+            ],
+          );
+        },
         splashColor: Colors.blue.withAlpha(30),
         child: SizedBox(
           height: 80.0,
@@ -18,7 +54,7 @@ class CardManager extends StatelessWidget {
               child: Container(
                 alignment: Alignment.centerLeft,
                 child: Text(
-                  title,
+                  '${account.name} (${account.email})',
                   style: TextStyle(fontSize: 20.0),
                 ),
               ),
