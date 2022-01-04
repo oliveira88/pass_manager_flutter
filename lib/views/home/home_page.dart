61e05baf9c76563d37708533986b6b694d1341ed
Guilherme Oliveira <gui.r.oliveira.88@gmail.com>
2022-01-03 23:42:46 -0300

    Mudança na home page para utilizar os commands

--- lib/views/home/home_page.dart
+++ lib/views/home/home_page.dart
@@ -1,63 +1,95 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:pass_manager/model/account_list.dart';
+import 'package:pass_manager/commands/account/get_accounts_command.dart';
+import 'package:pass_manager/dto/account_dto.dart';
 import 'package:pass_manager/views/add_account/add_account_page.dart';
 import 'package:pass_manager/views/card_manager/card_manager_widget.dart';
 import 'package:provider/provider.dart';
 
-class HomePage extends StatelessWidget {
+import '../../model/app_model.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
-    final accounts = Provider.of<AccountList>(context);
-    return Observer(
-      builder: (_) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Text('Gerenciador de Senhas'),
-          ),
-          body: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: accounts.accounts.isEmpty
-                ? Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Icon(
-                          Icons.people,
-                          color: Colors.black54,
-                          size: 80.0,
-                        ),
-                        SizedBox(height: 8.0),
-                        Text(
-                          'Nenhuma conta adicionada!',
-                          style:
-                              TextStyle(fontSize: 18.0, color: Colors.black54),
-                          textAlign: TextAlign.center,
-                        ),
-                      ],
-                    ),
-                  )
-                : ListView.builder(
-                    itemCount: accounts.accounts.length,
-                    itemBuilder: (_, index) {
-                      final account = accounts.accounts[index];
-                      return CardManager(account: account);
-                    },
-                  ),
-          ),
-          floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.add),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => AddPassword()),
-              );
-            },
-          ),
+    var accountList =
+        context.select<AppModel, List<AccountDto>>((a) => a.accountList);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Gerenciador de Senhas'),
+      ),
+      body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: accountList.isEmpty ? EmptyState() : AccountsList()),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AddPassword()),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class AccountsList extends StatelessWidget {
+  const AccountsList({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<AccountDto>>(
+      future: GetAccountsCommand().execute(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        return ListView.builder(
+          itemCount: snapshot.data!.length,
+          itemBuilder: (_, index) {
+            final account = snapshot.data![index];
+            return CardManager(account: account);
+          },
         );
       },
+    );
+  }
+}
+
+class EmptyState extends StatelessWidget {
+  const EmptyState({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Icon(
+            Icons.people,
+            color: Colors.black54,
+            size: 80.0,
+          ),
+          SizedBox(height: 8.0),
+          Text(
+            'Nenhuma conta adicionada!',
+            style: TextStyle(fontSize: 18.0, color: Colors.black54),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
     );
   }
 }
